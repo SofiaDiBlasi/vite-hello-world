2711e5a62f09009e0f7fe0ce6f331a902f6af98c
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-05-18 16:15:10 +0100

    Milestone 1-2

---
+++ src/components/AppCat.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        name: "AppCat"
+    }
+</script>
+
+<template>
+    <div>
+        <img src="../assets/Java.jpg" alt="">
+    </div>
+</template>
+
+<style scoped>
+img {
+    border: pink 1px solid;
+    height: 750px;
+    width: 650px;
+}
+</style>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,15 @@
+<script>
+//Definiamo il nome del componente ed eventualmente i componenti che usa (nessuno)
+export default {
+    name: "AppContent"
+}
+</script>
+
+<!-- Quando si importa questo componente verrà visualizzato questo HTML al suo posto -->
+<template>
+    <div>
+        <h2>Proprio bella bella</h2>
+    </div>
+</template>
+
+<!-- Non c'è stile speicifoc per il componente, usiamo solo quanto c'è in  style.css -->
